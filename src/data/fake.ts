883ed887d913542d1ab1a56c38971b019692d0ec
George <gogent01@gmail.com>
2023-03-08 11:28:47 +0100

    Now faker generates sex-correct russian fullnames

--- src/data/fake.ts
+++ src/data/fake.ts
@@ -1,4 +1,5 @@
 import { faker } from '@faker-js/faker';
+faker.setLocale('ru');
 
 type FakeQueryColumn = {
   key: string;
@@ -20,13 +21,15 @@ type ReactiveColumn = {
 export type ReactiveSchema = (FakeQueryColumn & ReactiveColumn)[];
 
 function createRandomRow(id: number): FakeQueryRow {
+  const gender = faker.helpers.arrayElement(['Мужской', 'Женский']);
+  const fullname = gender === 'Мужской' ? faker.name.fullName({ sex: 'male' }) : faker.name.fullName({ sex: 'female' });
   const dateOfBirth = faker.date.birthdate({ min: 18, max: 85, mode: 'age' });
   const height = parseFloat(faker.finance.amount(140, 200, 1));
   const weight = parseFloat(faker.finance.amount(40, 140, 2));
   return {
     center: faker.helpers.arrayElement(['МКНЦ им. А.С. Логинова', 'ГКОБ 1', 'ГКОБ 62']),
-    fullname: faker.name.fullName(),
-    gender: faker.helpers.arrayElement(['Мужской', 'Женский']),
+    fullname,
+    gender,
     history: faker.random.alphaNumeric(5, { casing: 'upper' }),
     dateOfBirth: dateOfBirth,
     age: new Date().getFullYear() - new Date(dateOfBirth).getFullYear(),
